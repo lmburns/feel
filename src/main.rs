ecd08718282dab696dae219b8d0b294f71c801ed
Lucas Burns <burnsac@me.com>
2021-08-14 18:50:36 -0500

    added option to not overwrite files

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use colored::*;
 use filetime::FileTime;
 
 use std::{
-    fs::{DirBuilder, OpenOptions},
+    fs::{self, DirBuilder, OpenOptions},
+    io::{self, BufReader, Read, Write},
     path::PathBuf,
     time::SystemTime,
 };
@@ -32,11 +33,46 @@ struct FeelOpts {
     quiet: bool,
 }
 
+fn prompt_yes<T: AsRef<str>>(prompt: T) -> bool {
+    print!(
+        "{} [{}/{}] ",
+        prompt.as_ref(),
+        "y".green().bold(),
+        "N".red().bold()
+    );
+    if io::stdout().flush().is_err() {
+        // If stdout wasn't flushed properly, fallback to println
+        println!(
+            "{} [{}/{}]",
+            prompt.as_ref(),
+            "y".green().bold(),
+            "N".red().bold()
+        );
+    }
+    let stdin = BufReader::new(io::stdin());
+    stdin
+        .bytes()
+        .next()
+        .and_then(|c| c.ok())
+        .map(|c| c as char)
+        .map(|c| (c == 'y' || c == 'Y'))
+        .unwrap_or(false)
+}
+
 fn main() -> Result<(), String> {
     let opts = FeelOpts::parse();
 
     for (idx, path) in opts.path.iter().enumerate() {
         let dir = path.parent().expect("unable to find the path base");
+
+        if fs::metadata(path).is_ok()
+            && !prompt_yes(format!(
+                "File: {} exists, overwrite?",
+                path.display().to_string().bold().green()
+            ))
+        {
+            continue;
+        }
 
         DirBuilder::new()
             .recursive(true)
@@ -56,7 +92,7 @@ fn main() -> Result<(), String> {
             .map_err(|_| String::from("could not update file times"))?;
 
         if !opts.quiet {
-            if idx == 0 as usize {
+            if idx == 0_usize {
                 println!("{}", "Created".bold().green());
             }
             println!("  └  {}", path.display().to_string().bold().magenta());
